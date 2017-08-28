5d323d499078767f5e8cf92a3c53f3c3f96f35ba
王华君 <smart.dj@163.com>
2017-08-28 10:11:20 +0800

    添加计算字符串尺寸的计算，暂未实现

---
+++ DKit/Category/NSString/NSString+Size.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Size.h
+//  pangxie
+//
+//  Created by arthur on 2017/8/26.
+//  Copyright © 2017年 yuntongda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Size)
+
+@end
---
+++ DKit/Category/NSString/NSString+Size.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Size.m
+//  pangxie
+//
+//  Created by arthur on 2017/8/26.
+//  Copyright © 2017年 yuntongda. All rights reserved.
+//
+
+#import "NSString+Size.h"
+
+@implementation NSString (Size)
+
+@end
